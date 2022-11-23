3011e376665803258a4eebf7eddf2bc9736cb85a
martoast <alexmartos96@gmail.com>
2022-11-23 15:43:01 -0800

    navigate card

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -1,6 +1,7 @@
-import { Text, StyleSheet, View, SafeAreaView } from 'react-native'
+import { Text, StyleSheet, View, SafeAreaView, TouchableOpacity } from 'react-native'
 import React from 'react'
 import tw from 'tailwind-react-native-classnames';
+import { Icon } from "react-native-elements";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import { GOOGLE_MAPS_APIKEY } from "@env";
 import { useDispatch } from "react-redux";
@@ -48,6 +49,29 @@ const NavigateCard = () => {
               <NavFavourites />
   
           </View>
+          <View
+            style={tw`flex-row bg-white justify-evenly py-2 mt-auto border-t border-gray-100`}
+        >
+            <TouchableOpacity
+            onPress={() => navigation.navigate("RideOptionsCard")}
+            style={tw`flex flex-row justify-between bg-black w-24 px-4 py-3 rounded-full`}
+            >
+            <Icon name="car" type="font-awesome" color="white" size={16} />
+            <Text style={tw`text-white text-center`}>Rides</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+            style={tw`flex flex-row justify-between w-24 px-4 py-3 rounded-full`}
+            >
+            <Icon
+                name="fast-food-outline"
+                type="ionicon"
+                color="black"
+                size={16}
+            />
+            <Text style={tw`text-center`}>Eats</Text>
+            </TouchableOpacity>
+        </View>
         </SafeAreaView>
       )
 
